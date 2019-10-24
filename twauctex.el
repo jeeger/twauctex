8b3fb4c04728c78d694556f6d6a2d8dd26985090
Jan Seeger <jan.seeger@siemens.com>
2019-10-24 13:34:29 +0200

    Corrected variable watcher implementation.
    When left at the default value, the variable watcher was never called. Now we call it explicitly
    when initializing the mode.

--- twauctex.el
+++ twauctex.el
@@ -118,22 +118,22 @@ of an ampersand."
 (defvar twauctex--abbrev-regexp nil)
 (defvar twauctex--max-search-bound nil)
 
-(add-variable-watcher 'twauctex-non-break-abbrevs
-		      (lambda (symbol newval op where)
-			(when (eq op 'set)
-			  (setq twauctex--max-search-bound (or (cl-loop for abbrev in newval maximize (+ (length abbrev) 2))
-							       0)))))
+(defun twauctex--update-max-search-bound (symbol newval op where)
+  (when (eq op 'set)
+    (setq twauctex--max-search-bound (or (cl-loop for abbrev in newval maximize (+ (length abbrev) 2))
+					 0))))
 
-(add-variable-watcher 'twauctex-non-break-abbrevs
-		      (lambda (symbol newval op where)
-			(when (eq op 'set)
-			  (let ((split-regexp (regexp-opt (mapcar (lambda (c) (char-to-string c)) twauctex-electric-chars))))
-			    ;; Split abbreviations containing internal punctuation (like "M.Sc.").
-			    ;; We also need to match prefixes (like "M" for M.Sc.), so we don't electric-break the line when typing the abbreviation.
-			    (setq twauctex--abbrev-regexp
-				  (regexp-opt (apply #'append (mapcar (lambda (s) (split-string s split-regexp t)) newval))))))))
+(defun twauctex--update-abbrev-regexp (symbol newval op where)
+  (when (eq op 'set)
+    (let ((split-regexp (regexp-opt (mapcar (lambda (c) (char-to-string c)) twauctex-electric-chars))))
+      ;; Split abbreviations containing internal punctuation (like "M.Sc.").
+      ;; We also need to match prefixes (like "M" for M.Sc.), so we don't electric-break the line when typing the abbreviation.
+      (setq twauctex--abbrev-regexp
+	    (regexp-opt (apply #'append (mapcar (lambda (s) (split-string s split-regexp t)) newval)))))))
+  
 
-
+(add-variable-watcher 'twauctex-non-break-abbrevs 'twauctex--update-max-search-bound)
+(add-variable-watcher 'twauctex-non-break-abbrevs 'twauctex--abbrev-regexp)
 
 ;; OSPL code
 
@@ -255,6 +255,8 @@ If called with ARG, or already at end of line, kill the line instead."
     (auto-fill-mode -1)
     (visual-line-mode 1)
     (set (make-local-variable 'fill-nobreak-predicate) #'twauctex-dont-break-on-nbsp)
+    (twauctex--update-abbrev-regexp nil twauctex-non-break-abbrevs 'set nil)
+    (twauctex--update-max-search-bound nil twauctex-non-break-abbrevs 'set nil)
     ;; We use hack-local-variables, because we want to take the
     ;; file-local fill column into account when setting the visual
     ;; fill column.
